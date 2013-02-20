edaedb860b1d9d43310fb3a377d650c1ac47bee6
Patrick Sturm <siyb@geekosphere.org>
2013-02-20 22:48:56 +0100

    [ADD] menu click are handled now

--- src/org/geekosphere/zeitgeist/activity/ZGActivity.java
+++ src/org/geekosphere/zeitgeist/activity/ZGActivity.java
@@ -4,10 +4,12 @@ import org.geekosphere.zeitgeist.R;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class ZGActivity extends SherlockFragmentActivity {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ZGActivity.class);
@@ -21,6 +23,18 @@ public class ZGActivity extends SherlockFragmentActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getSupportMenuInflater().inflate(R.menu.mainmenu, menu);
+
 		return super.onCreateOptionsMenu(menu);
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.mainmenu_settings:
+			startActivity(new Intent(this, ZGPreferenceActivity.class));
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
