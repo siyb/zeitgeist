6187b0d36387321ef198ef09645e4d244737a7a6
Patrick Sturm <siyb@geekosphere.org>
2013-07-10 22:19:56 +0200

    [FIX] tag array parsing

--- src/org/geekosphere/zeitgeist/processor/ZGTagProcessor.java
+++ src/org/geekosphere/zeitgeist/processor/ZGTagProcessor.java
@@ -8,27 +8,29 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import at.diamonddogs.service.processor.JSONArrayProcessor;
+import at.diamonddogs.service.processor.JSONProcessor;
 
-public class ZGTagProcessor extends JSONArrayProcessor<ZGTag[]> {
+public class ZGTagProcessor extends JSONProcessor<ZGTag[]> {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ZGTagProcessor.class);
 	public static final int ID = 23485234;
 
 	@Override
-	protected ZGTag[] parse(JSONArray inputObject) {
+	protected ZGTag[] parse(JSONObject inputObject) {
 		ArrayList<ZGTag> ret = new ArrayList<ZGTag>();
-		ZGTag tmp;
-		for (int i = 0; i < inputObject.length(); i++) {
-			try {
+		try {
+			ZGTag tmp;
+			JSONArray array = inputObject.getJSONArray("tags");
+			for (int i = 0; i < inputObject.length(); i++) {
 				tmp = new ZGTag();
-				JSONObject o = (JSONObject) inputObject.get(i);
+				JSONObject o = (JSONObject) array.get(i);
 				tmp.setId(o.getInt("id"));
 				tmp.setItemCount(o.getInt("count"));
 				tmp.setTagName(o.getString("tagname"));
 				ret.add(tmp);
-			} catch (Throwable tr) {
-				LOGGER.debug("Could not parse tag.", tr);
 			}
+		} catch (Throwable tr) {
+			LOGGER.debug("Could not parse tag.", tr);
+			return null;
 		}
 		return ret.toArray(new ZGTag[ret.size()]);
 	}
