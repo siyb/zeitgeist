9fa90e5cbb5ecdd18c5b69e35f3a050f3da4721f
Patrick Sturm <siyb@geekosphere.org>
2013-07-11 23:46:41 +0200

    [ADD] tag search fully implemented

--- src/org/geekosphere/zeitgeist/activity/ZGTagQueryTextListener.java
+++ src/org/geekosphere/zeitgeist/activity/ZGTagQueryTextListener.java
@@ -8,13 +8,10 @@ import org.geekosphere.zeitgeist.view.adapter.TagSuggestionAdapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.MatrixCursor;
-import android.os.Handler;
-import android.os.Message;
 import android.widget.FilterQueryProvider;
 import at.diamonddogs.data.dataobjects.CacheInformation;
 import at.diamonddogs.data.dataobjects.WebRequest;
 import at.diamonddogs.service.net.HttpServiceAssister;
-import at.diamonddogs.service.processor.ServiceProcessor;
 
 import com.actionbarsherlock.widget.SearchView;
 import com.actionbarsherlock.widget.SearchView.OnQueryTextListener;
@@ -30,41 +27,35 @@ public class ZGTagQueryTextListener implements OnQueryTextListener {
 		this.context = c;
 		this.assister = assister;
 		this.sv = sv;
+		loadTags();
 		sv.setOnQueryTextListener(this);
+		adapter = new TagSuggestionAdapter(context, createCursorFromQuery(""));
+		adapter.setFilterQueryProvider(new FilterQueryProvider() {
+			@Override
+			public Cursor runQuery(CharSequence constraint) {
+				return createCursorFromQuery(constraint == null ? "" : constraint.toString());
+			}
+		});
+		sv.setSuggestionsAdapter(adapter);
+	}
+
+	private void loadTags() {
+		WebRequestBuilder b = new WebRequestBuilder(context);
+		WebRequest wr = b.getTags().build();
+		wr.setCacheTime(CacheInformation.CACHE_1H);
+		tags = (ZGTag[]) assister.runSynchronousWebRequest(wr, new ZGTagProcessor()).getPayload();
 	}
 
 	@Override
 	public boolean onQueryTextSubmit(String query) {
+
 		return false;
 	}
 
 	@Override
 	public boolean onQueryTextChange(String newText) {
-		WebRequestBuilder b = new WebRequestBuilder(context);
-		WebRequest wr = b.getTags().build();
-		wr.setCacheTime(CacheInformation.CACHE_1H);
-		assister.runWebRequest(new TagDownloadHandler(), wr, new ZGTagProcessor());
-		return true;
-	}
-
-	private final class TagDownloadHandler extends Handler {
-		@Override
-		public void handleMessage(Message msg) {
-			if (msg.what == ZGTagProcessor.ID) {
-				if (msg.arg1 == ServiceProcessor.RETURN_MESSAGE_OK) {
-					tags = (ZGTag[]) msg.obj;
-
-					adapter = new TagSuggestionAdapter(context, null);
-					adapter.setFilterQueryProvider(new FilterQueryProvider() {
-						@Override
-						public Cursor runQuery(CharSequence constraint) {
-							return createCursorFromQuery(constraint.toString());
-						}
-					});
-					sv.setSuggestionsAdapter(adapter);
-				}
-			}
-		}
+		adapter.getFilterQueryProvider().runQuery(newText);
+		return false;
 	}
 
 	private Cursor createCursorFromQuery(String query) {
