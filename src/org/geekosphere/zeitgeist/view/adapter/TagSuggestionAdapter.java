bf4af734b69bc60da236ffbe47d1c04c2c055415
Patrick Sturm <siyb@geekosphere.org>
2013-07-12 00:00:29 +0200

    [CHG] style of suggestions changed

--- src/org/geekosphere/zeitgeist/view/adapter/TagSuggestionAdapter.java
+++ src/org/geekosphere/zeitgeist/view/adapter/TagSuggestionAdapter.java
@@ -1,8 +1,11 @@
 package org.geekosphere.zeitgeist.view.adapter;
 
+import org.geekosphere.zeitgeist.R;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,12 +18,20 @@ public class TagSuggestionAdapter extends CursorAdapter {
 
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		((TextView) view).setText(cursor.getString(cursor.getColumnIndexOrThrow("tag")));
+		ViewHolder vh = (ViewHolder) view.getTag();
+		vh.tagName.setText(cursor.getString(cursor.getColumnIndexOrThrow("tag")));
 	}
 
 	@Override
 	public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-		return new TextView(context);
+		View v = LayoutInflater.from(context).inflate(R.layout.list_item_tagsuggestionadapter, viewGroup, false);
+		ViewHolder vh = new ViewHolder();
+		vh.tagName = (TextView) v.findViewById(R.id.list_item_tagsuggestionadapter_tv_display);
+		v.setTag(vh);
+		return v;
 	}
 
+	private static final class ViewHolder {
+		private TextView tagName;
+	}
 }
