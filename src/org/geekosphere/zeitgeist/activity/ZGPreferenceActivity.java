a7defc61ce15ac396b0b1bc994e363e807fe7755
Matthias H <apoc@sixserv.org>
2013-09-16 18:16:54 +0200

    [FIX] spaces>tabs code style fix

--- src/org/geekosphere/zeitgeist/activity/ZGPreferenceActivity.java
+++ src/org/geekosphere/zeitgeist/activity/ZGPreferenceActivity.java
@@ -18,13 +18,13 @@ import android.preference.PreferenceManager;
 
 public class ZGPreferenceActivity extends PreferenceActivity implements OnPreferenceClickListener {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ZGPreferenceActivity.class);
-	
+
 	public static final String KEY_INITIALIZED = "KEY_INITIALIZED";
 	public static final String KEY_HOST = "KEY_HOST";
 	public static final String KEY_EMAIL = "KEY_EMAIL";
 	public static final String KEY_API_SECRET = "KEY_API_SECRET";
 	public static final String KEY_SCAN_API_SECRET = "KEY_SCAN_API_SECRET";
-	
+
 	private EditTextPreference hostPreference;
 	private EditTextPreference emailPreference;
 	private EditTextPreference apiSecretPreference;
@@ -32,50 +32,49 @@ public class ZGPreferenceActivity extends PreferenceActivity implements OnPrefer
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		addPreferencesFromResource(R.xml.zgpreferenceactivity);
-		
+
 		hostPreference = (EditTextPreference) findPreference(KEY_HOST);
-		
+
 		emailPreference = (EditTextPreference) findPreference(KEY_EMAIL);
 		apiSecretPreference = (EditTextPreference) findPreference(KEY_API_SECRET);
 		findPreference(KEY_SCAN_API_SECRET).setOnPreferenceClickListener(this);
 	}
-	
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (scanResult != null && scanResult.getContents() != null) {
-            String qrcode = scanResult.getContents();
-            LOGGER.info("qrcode scan success: " + qrcode);
-            
-            // parse qrcode into url, email and secret:
-            int auth_token = qrcode.indexOf("#auth:");
-            if (auth_token != -1) {
-                String baseUrl = qrcode.substring(0, auth_token);
-                String eMail = qrcode.substring(auth_token + 6, qrcode.indexOf('|'));
-                String apiSecret = qrcode.substring(qrcode.indexOf('|') + 1);
-                
-                LOGGER.debug("qrcode baseUrl: " + baseUrl);
-                LOGGER.debug("qrcode eMail: " + eMail);
-                
-                hostPreference.setText(baseUrl);
-                emailPreference.setText(eMail);
-                apiSecretPreference.setText(apiSecret);
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+		if (scanResult != null && scanResult.getContents() != null) {
+			String qrcode = scanResult.getContents();
+			LOGGER.info("qrcode scan success: " + qrcode);
 
-                LOGGER.info("Successfully changed baseUrl, eMail and apiSecret!");
-            }
-            else {
-            	LOGGER.info("QR Code with invalid token scanned: " + qrcode);
-            }
-        }
-    }
-	
+			// parse qrcode into url, email and secret:
+			int auth_token = qrcode.indexOf("#auth:");
+			if (auth_token != -1) {
+				String baseUrl = qrcode.substring(0, auth_token);
+				String eMail = qrcode.substring(auth_token + 6, qrcode.indexOf('|'));
+				String apiSecret = qrcode.substring(qrcode.indexOf('|') + 1);
+
+				LOGGER.debug("qrcode baseUrl: " + baseUrl);
+				LOGGER.debug("qrcode eMail: " + eMail);
+
+				hostPreference.setText(baseUrl);
+				emailPreference.setText(eMail);
+				apiSecretPreference.setText(apiSecret);
+
+				LOGGER.info("Successfully changed baseUrl, eMail and apiSecret!");
+			}
+			else {
+				LOGGER.info("QR Code with invalid token scanned: " + qrcode);
+			}
+		}
+	}
+
 	@Override
 	public boolean onPreferenceClick(Preference pref) {
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.initiateScan();
-        return false;
+		IntentIntegrator integrator = new IntentIntegrator(this);
+		integrator.initiateScan();
+		return false;
 	}
 }
